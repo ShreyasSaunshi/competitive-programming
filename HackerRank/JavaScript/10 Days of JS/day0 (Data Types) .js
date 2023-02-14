46a61db431dca82e77c4c6fac7d34faf242ae437
Shreyas Saunshi <67939784+ShreyasSaunshi@users.noreply.github.com>
2023-02-13 20:08:38 -0800

    Update day0 (Data Types) .js

--- HackerRank/JavaScript/10 Days of JS/day0 (Data Types) .js
+++ HackerRank/JavaScript/10 Days of JS/day0 (Data Types) .js
@@ -1,5 +1,10 @@
 function performOperation(secondInteger, secondDecimal, secondString) {
-    const firstInteger = 4;
-    const firstDecimal = 4.0;
-    const firstString = 'HackerRank ';
+     const firstInteger = 4;
+     const firstDecimal = 4.0;
+     const firstString = 'HackerRank ';
+     
+     // write code below
+     console.log((firstInteger + parseInt(secondInteger, 0)));
+     console.log((firstDecimal + parseFloat(secondDecimal, 2)));
+     console.log(`${firstString}${secondString}`); 
 }
