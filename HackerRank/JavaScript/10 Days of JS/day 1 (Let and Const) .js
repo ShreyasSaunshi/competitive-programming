5917c0386cf0cc02a29d6c9c85993676f41ef70c
Shreyas Saunshi <67939784+ShreyasSaunshi@users.noreply.github.com>
2023-02-13 20:10:11 -0800

    Create day 1 (Let and Const) .js

---
+++ HackerRank/JavaScript/10 Days of JS/day 1 (Let and Const) .js
@@ -0,0 +1,10 @@
+...
+try {    
+    // Attempt to redefine the value of constant variable PI
+    PI = 0;
+    // Attempt to print the value of PI
+    console.log(PI);
+} catch(error) {
+    console.error("You correctly declared 'PI' as a constant.");
+}
+...
